eeaefb01672b2befc90f3888dc925ae740eafaa0
Audrey Cipres <audreycipres123@gmail.com>
2022-06-09 17:20:01 +1000

    shouting strings

--- set2/exercise0.py
+++ set2/exercise0.py
@@ -17,6 +17,9 @@ obvious answer, that's because it is!
 """
 
 
+from git import UpdateProgress
+
+
 def add_1(a_number):
     """Return a number that is 1 bigger than number given.
 
@@ -66,7 +69,7 @@ def shout(a_string):
           they all do different things. You'll need to actually read the
           docs to find out which one you actually need.
     """
-    the_answer = None
+    the_answer = a_string.upper()
     return the_answer
 
 
@@ -79,7 +82,7 @@ def really_shout(a_string):
     that you've reused the function you already wrote.
     Look up how to 'concatinate' strings to make this happen.
     """
-    the_answer = None
+    the_answer = a_string.upper() + "!"
     return the_answer
 
 
@@ -95,7 +98,8 @@ def shout_with_a_number(a_string, a_number):
           challenge, see if you can make the test pass with at least two ways
           of doing the same job.
     """
-    the_answer = None
+    shout_string = shout(a_string)
+    the_answer = f" {shout_string} {a_number}
     return the_answer
 
 
--- set2/readme.md
+++ set2/readme.md
@@ -1,1 +1,2 @@
 TODO: Reflect on what you learned this week and what is still unclear.
+string.upper() converts a string to uppercase
